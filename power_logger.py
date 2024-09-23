73bfc6880b70fbe31216302c2d4f3ab936245cbc
Magnus Beischer <mb@auto-maskin.com>
2024-09-23 02:23:56 +0200

    Just auto increment missing.

--- power_logger.py
+++ power_logger.py
@@ -32,22 +32,27 @@ def log_to_database(data):
 
         # Insert data into the "log" table
         wifi_strength = data["wifi_strength"]
-        total_power_import_kwh = data["total_power_import_kwh"]
-        active_power_w = data["active_power_w"]
-        active_power_l1_w = data["active_power_l1_w"]
-        active_power_l2_w = data["active_power_l2_w"]
-        active_power_l3_w = data["active_power_l3_w"]
-        active_current_a = data["active_current_a"]
-        active_current_l1_a = data["active_current_l1_a"]
-        active_current_l2_a = data["active_current_l2_a"]
-        active_current_l3_a = data["active_current_l3_a"]
+        total_power_import_kwh = float(data["total_power_import_kwh"])
+        active_power_w = float(data["active_power_w"])
+        active_power_l1_w = float(data["active_power_l1_w"])
+        active_power_l2_w = float(data["active_power_l2_w"])
+        active_power_l3_w = float(data["active_power_l3_w"])
+        active_current_a = float(data["active_current_a"])
+        active_current_l1_a = float(data["active_current_l1_a"])
+        active_current_l2_a = float(data["active_current_l2_a"])
+        active_current_l3_a = float(data["active_current_l3_a"])
+        active_voltage_l1_v = float(data["active_voltage_l1_v"])
+        active_voltage_l2_v = float(data["active_voltage_l2_v"])
+        active_voltage_l3_v = float(data["active_voltage_l3_v"])
 
         sql = ("INSERT INTO log ( time, wifi_strength, total_power_import_kwh, active_power_w, active_power_l1_w, \
                active_power_l2_w, active_power_l3_w, active_current_a, active_current_l1_a, active_current_l2_a, \
-               active_current_l3_a) VALUES (%s, %s, %s, %s,  %s, %s, %s, %s, %s, %s, %s)")
+               active_current_l3_a, active_voltage_l1_v, active_voltage_l2_v, active_voltage_l3_v) \
+               VALUES (%s, %s, %s, %s,  %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
         values = (formatted_time, wifi_strength, total_power_import_kwh, active_power_w, active_power_l1_w,
                   active_power_l2_w, active_power_l3_w, active_current_a, active_current_l1_a,
-                  active_current_l1_a, active_current_l1_a)
+                  active_current_l2_a, active_current_l3_a, active_voltage_l1_v, active_voltage_l2_v,
+                  active_voltage_l3_v)
         cur.execute(sql, values)
         # print("SQL and values:", sql, values)
         # Commit the changes
